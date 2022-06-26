414ae3ffce32fa6eb472878cf629a9671fe8d17a
Malik <permalik@protonmail.com>
2022-06-26 15:21:33 -0500

    Complete 2.6

---
+++ phonebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newPerson = {
+      name: `${newName}`
+    };
+    setPersons((array) => [...array, newPerson]);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
