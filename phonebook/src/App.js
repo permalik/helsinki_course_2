abac01d57812a60fd72fa0392b82fd53c5cf221b
Malik <permalik@protonmail.com>
2022-06-26 20:37:27 -0500

    Complete 2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: 1112223333 }
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let newPerson = {
-      name: `${newName}`
+      name: `${newName}`,
+      number: newNumber
     };
 
     const isAvailable = (object) =>
@@ -29,13 +33,18 @@ const App = () => {
           name: <input onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>
+            {person.name}: {person.number}
+          </li>
         ))}
       </ul>
     </div>
